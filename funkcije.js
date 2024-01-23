df7d6a6209679a4e39153e3642c8ecd019a449d4
anze10 <96385387+anze10@users.noreply.github.com>
2024-01-23 15:30:22 +0100

    Add files via upload
    ta js je taprau

--- funkcije.js
+++ funkcije.js
@@ -1,31 +1,3 @@
-import Chart from 'chart.js/auto'
-(async function () {
-    const data = [
-        { year: 2010, count: 10 },
-        { year: 2011, count: 20 },
-        { year: 2012, count: 15 },
-        { year: 2013, count: 25 },
-        { year: 2014, count: 22 },
-        { year: 2015, count: 30 },
-        { year: 2016, count: 28 },
-    ];
-
-    new Chart(
-        document.getElementById('acquisitions'),
-        {
-            type: 'bar',
-            data: {
-                labels: data.map(row => row.year),
-                datasets: [
-                    {
-                        label: 'Acquisitions by year',
-                        data: data.map(row => row.count)
-                    }
-                ]
-            }
-        }
-    );
-})();
 // Funkcija za prikazovanje obrazca
 function showForm(formId) {
     // Skrij vse obrazce
@@ -84,5 +56,3 @@ function showForm(formId) {
     });
     document.getElementById(formId).style.display = 'block';
 }
-
-
